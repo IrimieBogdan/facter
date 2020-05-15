4b6591e7a235aa42bbf079f9ff1e8025868f7590
Andrei Filipovici <andrei.filipovici@andreis-mbp.eth.tsr.corp.puppet.net>
2020-05-15 13:20:02 +0300

    (skip_failures_on_ng) external_fact_overrides_custom_fact_with_10000_weight_or_less.rb on ubuntu and osx

--- acceptance/tests/external_facts/external_fact_overrides_custom_fact_with_10000_weight_or_less.rb
+++ acceptance/tests/external_facts/external_fact_overrides_custom_fact_with_10000_weight_or_less.rb
@@ -1,9 +1,6 @@
 test_name "C100151: external fact overrides a custom fact of weight 10000 or less" do
   tag 'risk:high'
 
-  confine :except, :platform => /ubuntu/
-  confine :except, :platform => /osx/
-
   require 'facter/acceptance/user_fact_utils'
   extend Facter::Acceptance::UserFactUtils
 
